04e8c1ecccfdbe79c0634e0ef58755edd8187641
juancarlosasensio <jasensio@ufm.edu>
2020-03-23 13:19:06 +0000

    Add actions to TodosController, plus creat todos#index and #new

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,12 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :destroy]
 
   def index
     @todos = current_user.todos
+    @todo = Todo.new
   end
+
+  def show; end
 
   def new
     @todo = Todo.new
@@ -10,6 +14,7 @@ class TodosController < ApplicationController
 
   def create
     @todo = Todo.new(todo_params)
+    @todo.user_id = current_user.id
     if @todo.save
       redirect_to todos_path
     else
@@ -17,9 +22,19 @@ class TodosController < ApplicationController
     end
   end
 
+  def destroy
+    @todo.destroy
+    redirect_to
+  end
+
   private
 
-  def todo_params
-    params.require(:todo).permit(:name, :completed, )
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
+
+  def todo_params
+    params.require(:todo).permit(:name, :due_date, :completed)
+  end
+
 end
