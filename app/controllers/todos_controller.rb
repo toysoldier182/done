53e0dadf2e1db1c66d6946ec8d57148ce9421bae
juancarlosasensio <jasensio@ufm.edu>
2020-03-24 19:21:41 +0000

    Implement RSS feed reader and read from a JSON file

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,6 +4,16 @@ class TodosController < ApplicationController
   def index
     @todos = current_user.todos
     @todo = Todo.new
+    @wwr_url = 'https://weworkremotely.com/categories/remote-programming-jobs.rss'
+    @remote_ok_url = "https://remoteok.io/api"
+    rss_feed(@wwr_url)
+    remote_ok_serialized = open(@remote_ok_url).read
+    @remote_ok = JSON.parse(remote_ok_serialized)
+    @remote_ok_jobs = []
+    @remote_ok[1..@remote_ok.length].each do |job|
+      @remote_ok_jobs << job
+    end
+    @remote_ok_jobs
   end
 
   def show; end
@@ -26,6 +36,18 @@ class TodosController < ApplicationController
     @todo.destroy
     redirect_to
   end
+
+  def rss_feed(url)
+    if url.include? "https://weworkremotely"
+      open(url) do |rss|
+        @wwr_feed = RSS::Parser.parse(rss)
+      end
+    @wwr_feed
+    end
+  end
+
+  # def remote_ok(url)
+  # end
 
   private
 
